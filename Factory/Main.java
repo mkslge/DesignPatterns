e9477ca1ffdfc046dfb15beb380dca1803c3762b
Mark Seeliger <117680637+mkslge@users.noreply.github.com>
2025-06-16 07:04:51 -0400

    Create Main.java

---
+++ Factory/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Pizza pizza = PizzaFactory.createPizza("Cheese");
+        Pizza pizza2 = PizzaFactory.createPizza("Pepperoni");
+
+    }
+}
+
